eb35315e7918a05081decc56eefe0b59531cf863
ffarall <37149322+ffarall@users.noreply.github.com>
2019-06-06 20:05:32 -0300

    aseg

---
+++ 1. Interfaz gráfica/GUI.py
@@ -0,0 +1,19 @@
+import PyQt5 as pyqt
+
+class ElectroGUI:
+    '''
+    Class dedicated to interact with the user using PyQT.
+    '''
+
+    def __init__(self):
+        self.myList = []
+        self.myDic = {}
+
+    def myMethod(self, whatever):
+        if self.myList:
+            pass
+        elif self.myDic:
+            pass
+        else:
+            pass
+    
