e8e1694f87c911b4a0933ae2a089cbc39db03475
ffarall <37149322+ffarall@users.noreply.github.com>
2019-06-06 23:38:11 -0300

    asdlfn

---
+++ 2. Calculator/Calculator.py
@@ -0,0 +1,15 @@
+from scipy import signal
+
+class Calculator:
+    '''
+    Calculates the necessary data to plot graphs for first and second order filters.
+    - To calculate first order filters:
+
+    - To calculate second order filters:
+
+    '''
+
+    def __init__(self):
+
+        pass
+
